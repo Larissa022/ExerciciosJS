11972755e48834ae842900c50a2240daf11605af
Larissa Nascimento <113188584+Larissa022@users.noreply.github.com>
2023-12-14 23:25:06 -0300

    Update exercicio_03.js
    Atualização do exercício com  a inserção de funções JavaScript.

--- exercicio_03.js
+++ exercicio_03.js
@@ -1,15 +1,23 @@
 
 //Programa que calcula o preço de um produto de acordo com a forma de pagamento.
 
+function aplicarDesconto(valor, desconto){
+    return (valor - (valor * (desconto / 100)));
+}
+
+function aplicarJuros(valor, juros){
+    return (valor + (valor * (juros / 100)));
+}
+
 const precoEtiqueta = 100;
-const formaDePagamento = 3;
+const formaDePagamento = 4;
 
 if( formaDePagamento === 1){
-    console.log(precoEtiqueta - (precoEtiqueta * 0.1));
+    console.log(aplicarDesconto(precoEtiqueta, 10));
 }else if(formaDePagamento === 2){
-    console.log(precoEtiqueta -(precoEtiqueta * 0.15));
+    console.log(aplicarDesconto(precoEtiqueta, 15));
 }else if(formaDePagamento === 3){
     console.log(precoEtiqueta);
 }else{
-    console.log(precoEtiqueta + (precoEtiqueta * 0.1));
+    console.log(aplicarJuros(precoEtiqueta, 10));
 }
